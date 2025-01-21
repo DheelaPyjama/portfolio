cf0a0464a2d8f454909caf3e643b0c0dd5b529d8
Rohit Chakraborty <rohitchak68@gmail.com>
2025-01-21 17:06:56 +0530

    cleanup func

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-scroll'
+import { BookmarkIcon, BellIcon, Pencil1Icon, ArchiveIcon, FaceIcon } from '@radix-ui/react-icons'
 
 function Navbar() {
+  const [lastScrollY, setLastScrollY] = useState(0)
+  const [showNavbar, setShowNavbar] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      (currentScrollY > lastScrollY && currentScrollY > 50) ? setShowNavbar(true) : setShowNavbar(false) 
+      setLastScrollY(currentScrollY)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => {window.removeEventListener('scroll', handleScroll)}
+  }, []);
+
   return (
-    <nav className='fixed bottom-0 mb-4 bg-white shadow'>
+    <nav className={`fixed my-4 shadow-md p-4 rounded-full text-white bg-black bottom-0 left-[50%] flex justify-center z-10 transition-transform duration-300 ${
+      showNavbar ? 'translate-y-0' : '-translate-y-full'
+    }`}>
       <ul className='flex space-x-4 cursor-pointer'>
-        <li><Link to='landing' smooth>Landing</Link></li>
-        <li><Link to='projects' smooth>Projects</Link></li>
-        <li><Link to='blogs' smooth>Blogs</Link></li>
-        <li><Link to='bookmarks' smooth>Bookmarks</Link></li>
-        <li><Link to='about' smooth>About</Link></li>
+        <li><Link to='landing' smooth><FaceIcon /></Link></li>
+        <li><Link to='projects' smooth><Pencil1Icon /></Link></li>
+        <li><Link to='blogs' smooth><ArchiveIcon/></Link></li>
+        <li><Link to='bookmarks' smooth><BookmarkIcon /></Link></li>
+        <li><Link to='about' smooth><BellIcon /></Link></li>
       </ul>
     </nav>
   )
